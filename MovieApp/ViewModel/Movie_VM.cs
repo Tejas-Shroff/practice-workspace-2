aa04fa2a7fe371dd3d0263aa24f3d42b6b026e26
Iamneo <techadmin@iamneo.ai>
2023-11-16 05:42:39 +0000

    Backup Commit

--- MovieApp/ViewModel/Movie_VM.cs
+++ MovieApp/ViewModel/Movie_VM.cs
@@ -1,9 +1,9 @@
 namespace MovieApp.ViewModel{
     public class Movie_VM{
         
-        public string Name{get;set;}
-        public string Actors{get;set;}
-        public string Role{get;set;}
+        public string? Name{get;set;}
+        public string? Actors{get;set;}
+        public string? Role{get;set;}
         public int YearRelease{get;set;}
 
     }
