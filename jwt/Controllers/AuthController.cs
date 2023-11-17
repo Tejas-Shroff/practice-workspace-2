01e861a9e5892f3246cb58346eea6aef2af75294
Iamneo <techadmin@iamneo.ai>
2023-11-17 10:26:35 +0000

    Backup Commit

--- jwt/Controllers/AuthController.cs
+++ jwt/Controllers/AuthController.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace jwt.Controllers
 {
-    [Route("[controller]")
+    [Route("[controller]")]
     [ApiController]
     public class AuthController : ControllerBase {
 
@@ -17,9 +18,16 @@ namespace jwt.Controllers
             if(user is null){
                 return BadRequest("Invalid Client request");
             }
-            //code to access the username and password
-            if(user.UserName) =="Tom"  && user.Password =="Jerry"){
-                
+            // code to access the username and password
+            
+            if(user.UserName =="Tom"  && user.Password =="Jerry"){
+
+                var SecretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signingCredentials = new SigningCredentials(secretKey,SecurityAlgorithms.HmacSha256);
+                var tokenOptions = new JwtSecurityToken(
+                    
+                )
+
             }
         }
        
