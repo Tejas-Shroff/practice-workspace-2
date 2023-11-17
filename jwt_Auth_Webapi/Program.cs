af25886117a38c460c327f300e9548a6a9623a2e
Iamneo <techadmin@iamneo.ai>
2023-11-17 09:26:35 +0000

    Backup Commit

--- jwt_Auth_Webapi/Program.cs
+++ jwt_Auth_Webapi/Program.cs
@@ -1,3 +1,11 @@
+// Add these three lines
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateSchema = JwtBearerDefaults.AuthenticationSchema;
+    opt.DefaultAuthenticateSchema = JwtBearerDefaults.AuthenticationSchema;
+})
+    .AddJw
+
+
 
 var app = builder.Build();
 
