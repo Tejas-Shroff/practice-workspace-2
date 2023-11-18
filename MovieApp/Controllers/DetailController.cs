39c9e2e7ddbbcd8975a06527b270847d76422611
Iamneo <techadmin@iamneo.ai>
2023-11-18 05:50:30 +0000

    Backup Commit

--- MovieApp/Controllers/DetailController.cs
+++ MovieApp/Controllers/DetailController.cs
@@ -1,10 +1,67 @@
 
-//Controller Detail Controller
+// //Controller Detail Controller
 
-using MovieApp.Models;
-// using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using System;
+// using MovieApp.Models;
+// // using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Mvc;
+// using System;
+// // namespace MovieApp.Controllers{
+// //     [ApiController]
+// //     [Route("/[controller]")]
+// //     public class DetailController:ControllerBase{
+// //         MovieContext context=new MovieContext();
+// //         [HttpGet]
+// //         [Route("ListDetails")]
+// //         public  IActionResult Get(){
+// //             var data=from d in context.Details select new{
+// //                 d.Movie.Name,
+// //                 d.Actor,
+// //                 d.Role
+// //             };
+// //             return Ok(data);
+// //         }
+// //         [Route("MovieDetails/{id}")]
+// //         public IActionResult Get(int id)
+// //         {
+// //             var data=from d in context.Details where d.MovieId==d.Movie.Id select new{
+// //                 d.Actor,
+// //                 d.Role,
+// //                 d.Movie.Name,
+// //                 d.Movie.YearRelease
+ 
+ 
+// //             };
+// //             return Ok(data.ToList());
+// //         }
+// //         // [HttpGet]
+// //         // [Route("ListDetails")]
+// //         // public  IActionResult Get(){
+// //         //     var data=from m in context.Details select m;
+// //         //     return Ok(data);
+// //         // }
+// //         [HttpPost]
+// //         [Route("AddDetails")]
+// //         public IActionResult Post(Detail d){
+// //             if(ModelState.IsValid){
+// //                 try{
+// //                 context.Details.Add(d);
+// //                 context.SaveChanges();
+// //                 }
+// //                 catch(System.Exception ex){
+// //                     return BadRequest(ex.InnerException.Message);
+// //                 }
+// //             }
+// //             return Created("Details Added",d); 
+// //         }
+// //     }
+
+// // }
+
+
+
+
+
+
 // namespace MovieApp.Controllers{
 //     [ApiController]
 //     [Route("/[controller]")]
@@ -12,49 +69,40 @@ using System;
 //         MovieContext context=new MovieContext();
 //         [HttpGet]
 //         [Route("ListDetails")]
-//         public  IActionResult Get(){
+//         public IActionResult Get(){
 //             var data=from d in context.Details select new{
 //                 d.Movie.Name,
-//                 d.Actor,
+//                 d.Actors,
 //                 d.Role
-//             };
-//             return Ok(data);
-//         }
-//         [Route("MovieDetails/{id}")]
-//         public IActionResult Get(int id)
-//         {
-//             var data=from d in context.Details where d.MovieId==d.Movie.Id select new{
-//                 d.Actor,
-//                 d.Role,
-//                 d.Movie.Name,
-//                 d.Movie.YearRelease
- 
- 
 //             };
 //             return Ok(data.ToList());
 //         }
-//         // [HttpGet]
-//         // [Route("ListDetails")]
-//         // public  IActionResult Get(){
-//         //     var data=from m in context.Details select m;
-//         //     return Ok(data);
-//         // }
+//         [HttpGet]
+//         [Route("MovieDetails/{id}")]
+//         public IActionResult Get(int id){
+//             var data=from d in context.Details where d.MovieId==id select new{
+//             d.Actors,
+//             d.Role,
+//             d.Movie.Name,
+//             d.Movie.YearRelease
+//             };
+//             return Ok(data);
+//         }
 //         [HttpPost]
-//         [Route("AddDetails")]
+//         [Route("AddMovieDetails")]
 //         public IActionResult Post(Detail d){
 //             if(ModelState.IsValid){
 //                 try{
-//                 context.Details.Add(d);
-//                 context.SaveChanges();
-//                 }
-//                 catch(System.Exception ex){
+//                     context.Details.Add(d);
+//                     context.SaveChanges();
+//                 }catch(System.Exception ex){
 //                     return BadRequest(ex.InnerException.Message);
 //                 }
 //             }
-//             return Created("Details Added",d); 
+//             return Created("Details",d);
 //         }
-//     }
 
+//     }
 // }
 
 
@@ -62,45 +110,101 @@ using System;
 
 
 
-namespace MovieApp.Controllers{
+
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+ 
+namespace MoviesApp.Controllers
+{
     [ApiController]
     [Route("/[controller]")]
-    public class DetailController:ControllerBase{
-        MovieContext context=new MovieContext();
+    public class DetailController : ControllerBase
+    {
+        MovieContext context = new MovieContext();
         [HttpGet]
-        [Route("ListDetails")]
-        public IActionResult Get(){
-            var data=from d in context.Details select new{
-                d.Movie.Name,
-                d.Actors,
-                d.Role
-            };
-            return Ok(data.ToList());
+        [Route("DisplayMovieDetails/(id)")]
+ 
+        public IActionResult Get(int id)
+        {
+            var data = from d in context.Details where d.MovieId == id select new{
+                Artist = d.Actors, Role=d.Role,MovieName=d.Movie.Name,Year = d.Movie.YearReleased};
+                return Ok(data);
         }
         [HttpGet]
-        [Route("MovieDetails/{id}")]
-        public IActionResult Get(int id){
-            var data=from d in context.Details where d.MovieId==id select new{
-            d.Actors,
-            d.Role,
-            d.Movie.Name,
-            d.Movie.YearRelease
-            };
+        [Route("ListDetail")]
+ 
+        public IActionResult Get()
+        {
+            var data = from d in context.Details select new {MovieName=d.Movie.Name,Artist=d.Actors};
             return Ok(data);
         }
-        [HttpPost]
-        [Route("AddMovieDetails")]
-        public IActionResult Post(Detail d){
+        [HttpGet]
+        [Route("ListDetails/{id}")]
+ 
+        public IActionResult Get(int? id)
+        {
+            if(id==null){
+                return BadRequest("Id cannot be null");
+            }
+            var data = (from d in context.Details where d.MovieId ==id select d).FirstOrDefault();
+            if(id==null){
+                return NotFound($"Detail {id} not found");
+            }
+            return Ok(data);
+        }
+         [HttpPost]
+        [Route("AddDetails")]
+        public IActionResult Post([FromBody] Detail detail)
+        {
             if(ModelState.IsValid){
                 try{
-                    context.Details.Add(d);
+                    context.Details.Add(detail);
                     context.SaveChanges();
-                }catch(System.Exception ex){
+                }
+                catch(System.Exception ex){
                     return BadRequest(ex.InnerException.Message);
                 }
             }
-            return Created("Details",d);
+            return Created("Record Added",detail);
         }
-
+ 
+        [HttpPut]
+        [Route("EditDetails/id")]
+        public IActionResult Put(int id, Detail detail)
+        {
+            if(ModelState.IsValid)
+            {Detail detail1 = context.Details.Find(id);
+            detail1.DetailId = detail.DetailId;
+            detail1.Actors=detail.Actors;
+            detail1.MovieId=detail.MovieId;
+            detail1.Gender=detail.Gender;
+            detail1.Role=detail.Gender;
+            context.SaveChanges();
+            return Ok();
+            }  
+           
+            return BadRequest("Unable to Edit record");
+        }
+        [HttpDelete]
+        [Route("DeleteMovies/id")]
+        public IActionResult Delete(int id)
+        {
+            try{
+                    // var movie = context.Movies.Where(m=>m.Id==id);
+                    // if(movie.Count()!=0){
+                    //     throw new Exception("Cannot Delete Movie");
+                    // }
+                    var data = context.Details.Find(id);
+                    context.Details.Remove(data);
+                    return Ok();
+                }
+                catch(System.Exception ex){
+                    return BadRequest(ex.Message);
+                }
+        }
+ 
     }
 }
--- MovieShow/src/app/services/movieservice.service.ts
+++ MovieShow/src/app/services/movieservice.service.ts
@@ -3,6 +3,10 @@ import { IMovie } from '../model/imovie';
 import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Idetails } from '../model/idetails';
+import { HttpResponse } from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,21 +20,21 @@ export class MovieserviceService {
     return this.httpclient.get<any[]>(this.url+ '/ListMovies')
   }
   getMovie(id: number) : Observable<IMovie> {
-    return this.httpclient.get<IMovie>(this.url + '/ListMovies/' +id)
+    return this.httpclient.get<IMovie>(this.url + '/ListMovies/' +id).pipe(catchError(this.handleError));
   }
   httpOptions = {headers : new HttpHeaders({'content-type' : 'application/json'})}
   addMovie(moviedata : IMovie) : Observable<IMovie>{
-    return this.httpclient.post<IMovie>(this.url +'/AddMovie',moviedata,this.httpOptions)
+    return this.httpclient.post<IMovie>(this.url +'/AddMovie',moviedata,this.httpOptions).pipe(catchError(this.handleError));
   }
   editMovie(moviedata : IMovie) : Observable<IMovie> {
-    return this.httpclient.put<IMovie>(this.url + '/EditMovie/' + moviedata.id ,moviedata,this.httpOptions)
+    return this.httpclient.put<IMovie>(this.url + '/EditMovie/' + moviedata.id ,moviedata,this.httpOptions).pipe(catchError(this.handleError));
   }
 
   deleleMovie(id : number) : Observable<IMovie> {
-    return this.httpclient.delete<IMovie> (this.url + '/DeleteMovie/' + id)
+    return this.httpclient.delete<IMovie> (this.url + '/DeleteMovie/' + id).pipe(catchError(this.handleError));
   }
   addDetails(detailsdata : Idetails) : Observable <Idetails>{
-    return this.httpclient.post<Idetails>(this.url1 + '/AddDetails', detailsdata , this.httpOptions)
+    return this.httpclient.post<Idetails>(this.url1 + '/AddDetails', detailsdata , this.httpOptions).pipe(catchError(this.handleError));
   }
   // showMovies() : Observable<any[]>{
   //   return this.httpOptions.get<any[]>(this.url +'/ShowMovies')
