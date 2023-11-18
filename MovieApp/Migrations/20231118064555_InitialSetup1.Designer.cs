4bcccaddebf3fafcf39489d08cc42714cb6a11bb
Iamneo <techadmin@iamneo.ai>
2023-11-18 06:49:59 +0000

    Backup Commit

--- MovieApp/Migrations/20231118064555_InitialSetup1.Designer.cs
+++ MovieApp/Migrations/20231118064555_InitialSetup1.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieApp.Models;
 
@@ -10,9 +11,10 @@ using MovieApp.Models;
 namespace MovieApp.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20231118064555_InitialSetup1")]
+    partial class InitialSetup1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MovieApp/Migrations/20231118064555_InitialSetup1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApp.Migrations
+{
+    public partial class InitialSetup1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Actors",
+                table: "Details",
+                newName: "Actor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Actor",
+                table: "Details",
+                newName: "Actors");
+        }
+    }
+}
