dfd1f06335b702efd10de04e8332ba113824406e
Iamneo <techadmin@iamneo.ai>
2023-11-20 11:24:00 +0000

    Backup Commit

--- EmpAngular/src/app/services/employee-service.service.ts
+++ EmpAngular/src/app/services/employee-service.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { ApplicationRef, Injectable } from '@angular/core';
+import { ApplicationRef, Injectable, OnDestroy } from '@angular/core';
 import { error } from 'protractor';
 import { Observable, throwError } from 'rxjs';
 import {catchError} from 'rxjs/operators';
@@ -16,7 +16,7 @@ export class EmployeeServiceService {
 
   getAllEmployees() : Observable<any[]> {
 
-    return this.httpclient.get<any[]>(this.url + '/ListEmployees').pipe(catchError(this.handleError));
+    return this.httpclient.get<any[]>(this.url + '/ListEmployees').pipe(catchError(this.handleError)); 
 
 
   }
@@ -24,12 +24,26 @@ export class EmployeeServiceService {
   getEmployee(id : number) : Observable<IEmployee>{
     return this.httpclient.get<IEmployee>(this.url + '/ListEmployees' +id).pipe(catchError(this.handleError));
   }
+  
 
   httpOptions = {headers : new HttpHeaders ({'content-type' : 'application/json'})};
 
   addEmployee(employeedata : IEmployee ) : Observable<IEmployee>{
-    return this.httpclient.post<IEmployee>(this.url + '/AddEmployee' + employeedata + this.httpOptions)
+    return this.httpclient.post<IEmployee>(this.url + '/AddEmployee' + employeedata + this.httpOptions).pipe(catchError(this.handleError));
   }
+
+
+
+  editEmployee(employeedata : IEmployee) : Observable<IEmployee>{
+    return this.httpclient.put<IEmployee>(this.url + '/EditEmployee'+ employeedata.id + employeedata + this.httpOptions).pipe(catchError(this.handleError));
+  }
+
+
+
+  deleteEmployee(id : number) : Observable<IEmployee>{
+    return this.httpclient.delete<IEmployee>(this.url +'/DeleteEmployee' + id).pipe(catchError(this.handleError));
+  }
+
 
   handleError (error : HttpErrorResponse){
 
