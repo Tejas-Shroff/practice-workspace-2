63d6ab9246fcadb14601d7e66fcd97203ff323d3
Iamneo <techadmin@iamneo.ai>
2023-11-21 18:14:47 +0000

    Backup Commit

--- angularapppractice/src/app/create/create.component.ts
+++ angularapppractice/src/app/create/create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../services/movie.service';
+import { Movie } from '../model/movie';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -8,19 +10,32 @@ import { MovieService } from '../services/movie.service';
 })
 export class CreateComponent implements OnInit {
 
-  
+  moviedata : Movie = {
+    id : 0,
+    name : '',
+    yearRelease : 0,
+    rating : 0
+  }
 
-  Mdata : any[] =[]
+  Mdata : Movie[] =[]
 
-  constructor(private ms : MovieService) {
+  constructor(private ms : MovieService , private ar : ActivatedRoute , private r : Router) { }
 
-    this.ms.addMovie(this.Mdata).subscribe(
+  saveData(Mdata : Movie) : void {
+
+    this.moviedata = Mdata
+
+    this.ms.addMovie(this.moviedata).subscribe(
       () => {
 
+        alert("Record added Successfully")
+        this.r.navigate(['ListMovies'])
+        
       }
     )
-
   }
+
+  
 
   ngOnInit() {
   }
